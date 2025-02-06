0ca89a65a19e1582ef3ade83b301b0b4e6eca35a
AkshayKuchibhatla <akshaykuchibhatla@gmail.com>
2025-02-06 15:36:53 -0500

    Implemented LinkedList class.

--- Exercises/ch17/src/LinkedList.h
+++ Exercises/ch17/src/LinkedList.h
@@ -3,25 +3,98 @@
 #include <string>
 using namespace std;
 
-struct Node {
-    int cargo;
-    Node* nextNode;
-
-    Node();
-    Node(int c);
-    Node(int c, Node* n);
-    string to_str() const;
-};
-
-class LinkedList {
-    int numNodes;
-    Node* front;
+template <class T>
+class Node {
+    T cargo;
+    Node<T>* next;
 
     public:
-        LinkedList();
-
-        // Inserts a new node at the front of the linked list.
-        // @param c: the cargo of the new node
-        void insertFront(int c);
+        Node() {}
+        Node(T cargo) {
+            this->cargo = cargo;
+            this->next = nullptr;
+        }
+        // Creates a new node
+        // @param cargo: the data held by the new node
+        // @param nextNode: the address of the next node
+        Node(T cargo, Node<T>* nextNode) {
+            this->cargo = cargo;
+            this->next = nextNode;
+        }
+        T getCargo() {
+            return cargo;
+        }
+        Node<T>* getNext() {
+            return next;
+        }
+        string to_string() const {
+            return std::to_string(cargo);
+        }
 };
+
+template <class T>
+class LinkedList {
+    int numNodes; // Number of nodes in the list
+    Node<T>* head; // The front of the list
+    public:
+        // Default constructor
+        LinkedList() {
+            head = nullptr;
+            numNodes = 0;
+        }
+        // Returns if the list is empty or not
+        bool empty() {
+            return (head == nullptr);
+        }
+        // Returns the number of nodes in the list
+        int size() const {
+            return numNodes;
+        }
+        // Adds a node to the beginning of the list
+        // @param cargo: the data held by the new starting node
+        void addFirst(T cargo) {
+            Node<T>* n = new Node<T>(cargo, head);
+            head = n;
+            numNodes++;
+        }
+        // Removes the item at the front of the list and returns the data it contains
+        T removeFront() {
+            if (empty()) {
+                throw runtime_error("Error: cannot remove from an empty list.");
+            }
+            T frontCargo = head->cargo;
+            Node<T> front = head;
+            head = head->getNext();
+            delete front;
+            numNodes--;
+            return frontCargo;
+        }
+        string to_string() const {
+            Node<T>* node = head;
+            string s = "(";
+
+            while (node != nullptr) {
+                s += node->to_string();
+                node = node->getNext();
+                if (node != nullptr)
+                    s += ", ";
+            }
+            s += ")";
+            return s;
+        }
+        string to_string_reverse() const {
+            Node<T>* node = head;
+            string s = ")";
+
+            while (node != nullptr) {
+                s = Node<T>::to_string(node) + s;
+                node = node->getNext();
+                if (node != nullptr)
+                    s = ", " + s;
+            }
+            s += ")";
+            return s;
+        }
+};
+
 #endif

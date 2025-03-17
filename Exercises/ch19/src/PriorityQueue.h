e4414b950010b671a7c988a49bee934b80399ffe
AkshayKuchibhatla <akshaykuchibhatla@gmail.com>
2025-03-17 00:19:05 -0400

    Implemented ArrayPriorityQueue.

--- Exercises/ch19/src/PriorityQueue.h
+++ Exercises/ch19/src/PriorityQueue.h
@@ -1,5 +1,6 @@
 #include "ElknerLinkedList.h"
 #include <string>
+#define MAX_ITEMS 5
 using namespace std;
 
 template <class T>
@@ -20,7 +21,7 @@ class PriorityQueue : public LinkedList<T> {
         }
         T dequeue() {
             if (empty())
-                throw underflow_error("Error: cannot dequeue from empty queue.");
+                throw underflow_error("Error: Cannot dequeue from empty queue.");
             return LinkedList<T>::remove_from_front();
         }
         T top() {
@@ -39,5 +40,57 @@ class PriorityQueue : public LinkedList<T> {
             s += std::to_string(LinkedList<T>::get_item_at(i));
             s += ")";
             return s;
+        }
+};
+
+template <class T>
+class ArrayPriorityQueue {
+    private:
+        int first;
+        int last;
+        int length;
+        T items[MAX_ITEMS];
+    public:
+        ArrayPriorityQueue() {
+            first = 0; // first and last store where exactly the items are in the array
+            last = 0;
+            length = 0; // Number of elements in the list
+        }
+        void enqueue(const T& val) {
+            if ((last + 1) % MAX_ITEMS == first) {
+                throw overflow_error("Error: No more space in queue.");
+            }
+            items[last] = val;
+            int index1 = last;
+            last = (last + 1) % MAX_ITEMS;
+            length++;
+            if (length == 1) return;
+
+            int index2 = (index1 == 0) ? MAX_ITEMS - 1 : index1 - 1;
+            T tempItem;
+            int swapCounter = 1;
+            while (items[index1] > items[index2] && swapCounter < length) {
+                tempItem = items[index1];
+                items[index1] = items[index2];
+                items[index2] = tempItem;
+                swapCounter++;
+
+                index2 = (index2 == 0) ? MAX_ITEMS - 1 : index2 - 1;
+                index1 = (index1 == 0) ? MAX_ITEMS - 1 : index1 - 1;
+            }
+        }
+        T dequeue() {
+            if (empty())
+                throw underflow_error("Error: Cannot dequeue from empty queue.");
+            
+            int old1st = first;
+            first = (first + 1) % MAX_ITEMS;
+            return items[old1st];
+        }
+        T top() {
+            return items[first];
+        }
+        bool empty() const {
+            return (first == last);
         }
 };
--- Exercises/ch19/src/test_priorityqueue.cpp
+++ Exercises/ch19/src/test_priorityqueue.cpp
@@ -22,5 +22,23 @@ TEST_CASE("Test removal of items and top().") {
     q.enqueue(9);
     CHECK(q.dequeue() == 9);
     CHECK(q.top() == 7);
+    for (int i = 0; i < 4; i++) {
+        q.dequeue();
+    }
+    CHECK_THROWS_WITH(q.dequeue(), "Error: Cannot dequeue from empty queue.");
 }
 
+TEST_CASE("Test ArrayPriorityQueue adding, removal and top().") {
+    ArrayPriorityQueue<int> apq;
+    apq.enqueue(4);
+    apq.enqueue(7);
+    apq.enqueue(3);
+    apq.enqueue(2);
+    CHECK_THROWS_WITH(apq.enqueue(10), "Error: No more space in queue.");
+    CHECK(apq.top() == 7);
+    CHECK(apq.dequeue() == 7);
+    CHECK(apq.dequeue() == 4);
+    CHECK(apq.dequeue() == 3);
+    CHECK(apq.dequeue() == 2);
+    CHECK_THROWS_WITH(apq.dequeue(), "Error: Cannot dequeue from empty queue.");
+}
